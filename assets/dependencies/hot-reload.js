e1a9eae38abbbe19a1cfdfde992dd866bac9de90
Adam <xadamxk@gmail.com>
2019-09-29 20:18:54 -0500

    Update hot-reload.js

--- assets/dependencies/hot-reload.js
+++ assets/dependencies/hot-reload.js
@@ -1,52 +1,37 @@
-const filesInDirectory = dir => new Promise (resolve =>
-
-    dir.createReader ().readEntries (entries =>
-
-        Promise.all (entries.filter (e => e.name[0] !== '.').map (e =>
-
+const filesInDirectory = dir => new Promise(resolve =>
+    dir.createReader().readEntries(entries =>
+        Promise.all(entries.filter(e => e.name[0] !== '.').map(e =>
             e.isDirectory
-                ? filesInDirectory (e)
-                : new Promise (resolve => e.file (resolve))
+                ? filesInDirectory(e)
+                : new Promise(resolve => e.file(resolve))
         ))
-        .then (files => [].concat (...files))
-        .then (resolve)
+            .then(files => [].concat(...files))
+            .then(resolve)
     )
 )
 
 const timestampForFilesInDirectory = dir =>
-        filesInDirectory (dir).then (files =>
-            files.map (f => f.name + f.lastModifiedDate).join ())
-
+    filesInDirectory(dir).then(files =>
+        files.map(f => f.name + f.lastModifiedDate).join())
 const reload = () => {
-
-    chrome.tabs.query ({ active: true, currentWindow: true }, tabs => { // NB: see https://github.com/xpl/crx-hotreload/issues/5
-
-        if (tabs[0]) { chrome.tabs.reload (tabs[0].id) }
-
-        chrome.runtime.reload ()
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => { // NB: see https://github.com/xpl/crx-hotreload/issues/5
+        if (tabs[0]) { chrome.tabs.reload(tabs[0].id) }
+        chrome.runtime.reload()
     })
 }
 
 const watchChanges = (dir, lastTimestamp) => {
-
-    timestampForFilesInDirectory (dir).then (timestamp => {
-
+    timestampForFilesInDirectory(dir).then(timestamp => {
         if (!lastTimestamp || (lastTimestamp === timestamp)) {
-
-            setTimeout (() => watchChanges (dir, timestamp), 1000) // retry after 1s
-
+            setTimeout(() => watchChanges(dir, timestamp), 1000) // retry after 1s
         } else {
-
-            reload ()
+            reload()
         }
     })
-
 }
 
-chrome.management.getSelf (self => {
-
+chrome.management.getSelf(self => {
     if (self.installType === 'development') {
-
-        chrome.runtime.getPackageDirectoryEntry (dir => watchChanges (dir))
+        chrome.runtime.getPackageDirectoryEntry(dir => watchChanges(dir))
     }
 })

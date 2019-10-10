dd0ca7d40527ff054ef25698f829b1bd7c042595
Adam <xadamxk@gmail.com>
2019-10-09 22:19:45 -0500

    Conditional to run BatteryPercent module

--- src/modules/game/BatteryPercent.js
+++ src/modules/game/BatteryPercent.js
@@ -11,12 +11,14 @@ class BatteryPercent extends HFX.Feature {
   }
 
   run() {
-    var batteryPercent = (
-      $(".game-top-right-icons > a:eq(4)")
-        ? parseInt($(".game-top-right-icons > a:eq(4)").attr("title").replace("%", ""))
-        : 0);
-    var batteryElement = $("[class^='hficon-battery-']").filter((index, item) => { return $(item).attr('href') });
-    batteryElement.after($("<span>").css({ "margin-left": "10px", "color": batteryElement.css('color') }).text(batteryPercent + "%"));
+    if ($("#game_content_currentpage").length > 0) {
+      var batteryPercent = (
+        $(".game-top-right-icons > a:eq(4)")
+          ? parseInt($(".game-top-right-icons > a:eq(4)").attr("title").replace("%", ""))
+          : 0);
+      var batteryElement = $("[class^='hficon-battery-']").filter((index, item) => { return $(item).attr('href') });
+      batteryElement.after($("<span>").css({ "margin-left": "10px", "color": batteryElement.css('color') }).text(batteryPercent + "%"));
+    }
   }
 };
 

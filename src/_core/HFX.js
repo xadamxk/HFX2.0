b9a02576df3b40a0d79502d5e94ff5961e8ef8a7
TyrantKingBen <42081911+TyrantKingBen@users.noreply.github.com>
2019-10-14 00:50:03 -0400

    Restructure HFX module
    Restructure HFX module so that further modules all have access to Logger/Util even in constructor.

--- src/_core/HFX.js
+++ src/_core/HFX.js
@@ -1,8 +1,11 @@
-global.HFX = {
-  Storage: require("./Storage"),
-  Logger: require("./Logger"),
-  Settings: require("./Settings"),
-  Section: require("./Section"),
-  Feature: require("./Feature"),
-  Util: require("./Util")
-};
+global.HFX = {};
+
+// Make sure Logger and Util are available immediately for other modules
+global.HFX.Logger = require("./Logger");
+global.HFX.Util = require("./Util");
+
+// Load other modules
+global.HFX.Storage = require("./Storage");
+global.HFX.Settings = require("./Settings");
+global.HFX.Section = require("./Section");
+global.HFX.Feature = require("./Feature");

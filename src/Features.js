6c6a87f95a817f7e35dd853251330cf08d156462
Adam <xadamxk@gmail.com>
2020-07-06 23:09:44 -0500

    Added feature files

--- src/Features.js
+++ src/Features.js
@@ -2,8 +2,6 @@ module.exports = {
   Alerts: require("./features/global/Alerts"),
   BatteryAbsoluteTime: require("./features/game/BatteryAbsoluteTime"),
   BatteryPercent: require("./features/game/BatteryPercent"),
-  ChangeBodyColor: require("./features/global/ChangeBodyColor"),
-  CollapsePossiblyRelatedThreads: require("./features/global/CollapsePossiblyRelatedThreads"),
   ExpandBlockedPosts: require("./features/threads/ExpandBlockedPosts"),
   HideBlockedPosts: require("./features/threads/HideBlockedPosts"),
   HideImages: require("./features/global/HideImages"),

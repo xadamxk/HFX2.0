60585ec14f5dcab107d094698224d07b721bbcca
Bennett Smith <smith.bennett@ic.edu>
2020-07-10 17:43:15 -0400

    Fix section names and automatic feature name generated by template.

--- src/core/Section.js
+++ src/core/Section.js
@@ -3,7 +3,7 @@ const Util = require("./Util");
 module.exports = class Section {
   constructor(...locations) {
     this.class = this.constructor.name;
-    this.name = this.class;
+    this.name = this.class.replace(/([a-z])([A-Z])/g, "$1 $2");
     this.locations = locations.length === 0 ? ["/"] : locations;
   }
 

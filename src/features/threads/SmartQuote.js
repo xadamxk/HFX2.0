e0906328f0a33f328027d2bebcccbd1cf67e4737
Adam <xadamxk@gmail.com>
2021-02-13 17:41:40 -0600

    Configuring default quote colors

--- src/features/threads/SmartQuote.js
+++ src/features/threads/SmartQuote.js
@@ -6,32 +6,40 @@ const Util = require("../../core/Util");
 
 class SmartQuote extends Feature {
   constructor() {
+    const defaultMentionColor = "#FF3B30";
+    const defaultQuoteColor = "#B1D8BF";
     super({
       section: Threads,
       name: "Smart Quote",
       default: true,
       description: "Adds styling and highlights mentioned quotes.",
       configurables: new ConfigurableArray(
-        new Color({id: "SQMentionColor", label: "Mention Color", default: "#FF3B30"}),
-        new Color({id: "SQColor", label: "Quote Color", default: "#B1D8BF"})
+        new Color({id: "SQMentionColor", label: "Mention Color", default: defaultMentionColor}),
+        new Color({id: "SQColor", label: "Quote Color", default: defaultQuoteColor})
       )
     });
   }
 
   run(settings) {
+    const username = $(".welcome > strong > a").text();
+    const mentionColor = Util.getConfigurableValue("SQMentionColor", this, settings) ? Util.getConfigurableValue("SQMentionColor", this, settings) : this.defaultMentionColor;
+    const quoteColor = Util.getConfigurableValue("SQColor", this, settings) ? Util.getConfigurableValue("SQColor", this, settings) : this.defaultQuoteColor;
+    // Loop quotes
     $("blockquote").each(function() {
-      console.log(this);
+      // blockquote
       $(this).css({
         "border-radius": "5px",
         "border": "1px solid black",
-        "padding": "1px 4px"
+        "padding": "1px 4px",
+        "margin-top": "10px"
       });
+
+      // blackquote header
+      const isMentioned = $(this).find("cite").text().includes(username);
       $(this).find("cite").css({
         "border-radius": "5px",
-        "padding": "2px 8px 2px 8px",
         "border": "1px solid black",
-        "": "",
-        "background": Util.getConfigurableValue("SQColor", this, settings)
+        "background": isMentioned ? mentionColor : quoteColor
       });
     });
   }

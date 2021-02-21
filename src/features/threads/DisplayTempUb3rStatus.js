c5e1316c18572bcadb345bf0673bbb4119291130
James <james@eli.cx>
2021-02-21 20:53:08 +0000

    Feature/display temp uber status (#88)
    * Display temp Ub3r status
    
    Display a user's temporary uber status on the postbit
    
    * Update DisplayTempUb3rStatus.js
    
    * Update DisplayTempUb3rStatus.js
    
    disabled by default, modified description, modify font and display
    
    Co-authored-by: Adam Koewler <xadamxk@gmail.com>

---
+++ src/features/threads/DisplayTempUb3rStatus.js
@@ -0,0 +1,29 @@
+const Feature = require("../../core/Feature");
+const Threads = require("../../sections/Threads");
+
+class DisplayTempUb3rStatus extends Feature {
+  constructor() {
+    super({
+      section: Threads,
+      name: "Display Temp Ub3r Status",
+      default: false,
+      description: "Displays the Ub3r expiration date below the awards for users who do not have permanent Ub3r.",
+      author: {
+        name: "James",
+        profile: "https://hackforums.net/member.php?action=profile&uid=2774521"
+      }
+    });
+  }
+
+  run() {
+    document.querySelectorAll("div.post_myawards > span > i[title*='Ub3r until']").forEach((award) => {
+      let uberStatus = document.createElement("span");
+      uberStatus.textContent = award.title;
+      uberStatus.style.fontSize = "13px";
+      uberStatus.style.display = "inline-block";
+      award.parentElement.parentElement.parentElement.append(uberStatus);
+    });
+  }
+};
+
+module.exports = new DisplayTempUb3rStatus();

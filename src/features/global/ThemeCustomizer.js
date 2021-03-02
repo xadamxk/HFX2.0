3a908bd0b895492144330f4ab1ef061df2fa28fd
Adam <xadamxk@gmail.com>
2021-03-01 21:15:29 -0600

    Updated theme with more colors

--- src/features/global/ThemeCustomizer.js
+++ src/features/global/ThemeCustomizer.js
@@ -3,6 +3,7 @@ const Global = require("../../sections/Global");
 
 const ConfigurableArray = require("../../core/ConfigurableArray");
 const Dropdown = require("../../configurables/Dropdown");
+const Checkbox = require("../../configurables/Checkbox");
 const Util = require("../../core/Util");
 
 class ThemeCustomizer extends Feature {
@@ -20,12 +21,18 @@ class ThemeCustomizer extends Feature {
           dropdownOptions: [
             {value: "default", label: "Default"},
             {value: "cyan", label: "Cyan"},
+            {value: "gray", label: "Gray"},
             {value: "magenta", label: "Magenta"},
+            {value: "orange", label: "Orange"},
+            {value: "orange2", label: "Orange2"},
             {value: "pink", label: "Pink"},
             {value: "pink2", label: "Pink2"},
+            {value: "twitterBlue", label: "Twitter Blue"},
             {value: "rainbow", label: "Rainbow"},
             {value: "yellow", label: "Yellow"}
-          ]})
+          ]}),
+        new Checkbox({ id: "tcChangeLogo", label: "Change Logo", default: true }),
+        new Checkbox({ id: "tcChangeMosaic", label: "Change Mosaic", default: true })
       )
     });
   }
@@ -42,10 +49,25 @@ class ThemeCustomizer extends Feature {
         mosaic = Util.getURL("/assets/images/theme-customizer/mosaics/mosaic_cyan.png");
         accentColor = "#2f525d";
         break;
+      case "gray":
+        logo = Util.getURL("/assets/images/theme-customizer/logos/logo_gray.png");
+        mosaic = Util.getURL("/assets/images/theme-customizer/mosaics/mosaic_gray.png");
+        accentColor = "#807e7e";
+        break;
       case "magenta":
         logo = Util.getURL("/assets/images/theme-customizer/logos/logo_magenta.gif");
         mosaic = Util.getURL("/assets/images/theme-customizer/mosaics/mosaic_magenta.png");
         accentColor = "#5d2f52";
+        break;
+      case "orange":
+        logo = Util.getURL("/assets/images/theme-customizer/logos/logo_orange.png");
+        mosaic = Util.getURL("/assets/images/theme-customizer/mosaics/mosaic_orange.png");
+        accentColor = "#bd5002";
+        break;
+      case "orange2":
+        logo = Util.getURL("/assets/images/theme-customizer/logos/logo_orange.png");
+        mosaic = Util.getURL("/assets/images/theme-customizer/mosaics/mosaic_orange2.png");
+        accentColor = "#803c0b";
         break;
       case "pink":
         logo = Util.getURL("/assets/images/theme-customizer/logos/logo_pink.png");
@@ -57,6 +79,11 @@ class ThemeCustomizer extends Feature {
         mosaic = Util.getURL("/assets/images/theme-customizer/mosaics/mosaic_pink2.png");
         accentColor = "#5d2f47";
         break;
+      case "twitterBlue":
+        logo = Util.getURL("/assets/images/theme-customizer/logos/logo_cyan.gif");
+        mosaic = Util.getURL("/assets/images/theme-customizer/mosaics/mosaic_cyan.png");
+        accentColor = "#1da1f2";
+        break;
       case "rainbow":
         logo = Util.getURL("/assets/images/theme-customizer/logos/logo_rainbow.gif");
         mosaic = Util.getURL("/assets/images/theme-customizer/mosaics/mosaic_rainbow.png");
@@ -65,18 +92,26 @@ class ThemeCustomizer extends Feature {
       case "yellow":
         logo = Util.getURL("/assets/images/theme-customizer/logos/logo_yellow.gif");
         mosaic = Util.getURL("/assets/images/theme-customizer/mosaics/mosaic_yellow.png");
-        accentColor = "#ffe14d";
+        accentColor = "#cab41b";
         break;
       default:
     }
 
     if (mosaic && logo && accentColor) {
       // logo
-      $(".logo-a > img").attr("src", logo);
+      if (Util.getConfigurableValue("tcChangeLogo", this, settings)) {
+        $(".logo-a > img").attr("src", logo);
+      }
       // mosaic
-      $("body").css("background", `#0e0e0e url(${mosaic}) fixed`);
+      if (Util.getConfigurableValue("tcChangeMosaic", this, settings)) {
+        $("body").css("background", `#0e0e0e url(${mosaic}) fixed`);
+      }
       // accent color
       $(".thead, .shadetabs li a.selected, .pagination .pagination_current, .quickthread_button").css("background", accentColor);
+      // $(".postbit_buttons > a, .postbit_buttons > .button").on("hover", function() {
+      //   console.log($(this));
+      //   $(this).css({"background-color": accentColor + " !important", "border-color": accentColor + " !important"});
+      // });
       $(".logo-hide-button").css("color", accentColor);
     }
   }

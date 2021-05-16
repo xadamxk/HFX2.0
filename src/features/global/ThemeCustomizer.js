10c29c8ed7f611d3758c5c9c134f323141170d7a
Adam <xadamxk@gmail.com>
2021-05-15 22:46:45 -0500

    Added Previous logos option for ThemeCustomizer
    Refactored storage access to use settings instead

--- src/features/global/ThemeCustomizer.js
+++ src/features/global/ThemeCustomizer.js
@@ -34,18 +34,63 @@ class ThemeCustomizer extends Feature {
           ]}),
         new Checkbox({ id: "tcChangeLogo", label: "Change Logo", default: true }),
         new Checkbox({ id: "tcChangeMosaic", label: "Change Mosaic", default: true }),
-        new Checkbox({ id: "tcBackgroundShadow", label: "Enable Background Shadow", default: false })
+        new Checkbox({ id: "tcBackgroundShadow", label: "Enable Background Shadow", default: false }),
+        new Dropdown({
+          id: "tcLogoImage",
+          label: "Previous Logos (Default: uses theme logo)",
+          default: "default",
+          dropdownOptions: [
+            new Option("default", "Default"),
+            new Option("https://hackforums.net/images/logo/logo-hf.png", "Black Convo"),
+            new Option("https://hackforums.net/images/logo/logo.10th.png", "10 Year Anniversary"),
+            new Option("https://hackforums.net/images/logo/logo.gwm.png", "Goodwill Monday"),
+            new Option("https://hackforums.net/images/logo/logo.hum.png", "Hackuman Release"),
+            new Option("https://hackforums.net/images/logo/logo.jpg", "HF Logo (Classic)"),
+            new Option("https://hackforums.net/images/logo/logo.pokemon.gif", "PokemonGo Release"),
+            new Option("https://hackforums.net/images/logo/logo.420.png", "HF 420"),
+            new Option("https://hackforums.net/images/logo/logo.xmas2014.gif", "Christmas (2014)"),
+            new Option("https://hackforums.net/images/logo/logo.xmas2015.gif", "Christmas (2015)"),
+            new Option("https://hackforums.net/images/logo/logo.xmas2017-2.png", "Christmas (2017)"),
+            new Option("https://hackforums.net/images/logo/logo.xmas2019.png", "Christmas (2019)"),
+            new Option("https://hackforums.net/images/logo/logo.4thJuly-3.png", "July 4th (Classic 1)"),
+            new Option("https://hackforums.net/images/logo/logo.4thJuly.png", "July 4th (Classic 2)"),
+            new Option("https://hackforums.net/images/logo/LOGO4TH.png", "July 4th (Modern)"),
+            new Option("https://hackforums.net/images/logo/logo.easter2019.png", "Easter (2019)"),
+            new Option("https://hackforums.net/images/logo/logo.halloween.png", "Halloween (Classic 1)"),
+            new Option("https://hackforums.net/images/logo/logo.halloween2.png", "Halloween (Classic 2)"),
+            new Option("https://hackforums.net/images/logo/logo.halloween3.gif", "Halloween (Classic 3)"),
+            new Option("https://hackforums.net/images/logo/logo.halloween4.gif", "Halloween (Classic 4)"),
+            new Option("https://hackforums.net/images/logo/logo.halloween5.gif", "Halloween (Classic 5)"),
+            new Option("https://hackforums.net/images/logo/logo.halloween-2017.png", "Halloween (2017)"),
+            new Option("https://hackforums.net/images/logo/logo.halloween2018.gif", "Halloween (2018)"),
+            new Option("https://hackforums.net/images/logo/logo-Halloween-2019.gif", "Halloween (2019)"),
+            new Option("https://hackforums.net/images/logo/logo.ny15.gif", "New Years (2015 1)"),
+            new Option("https://hackforums.net/images/logo/logo.nye15.png", "New Years (2015 2)"),
+            new Option("https://hackforums.net/images/logo/logo.newyear2016.gif", "New Years (2016 GIF)"),
+            new Option("https://hackforums.net/images/logo/logo.newyear2016.png", "New Years (2016 PNG)"),
+            new Option("https://hackforums.net/images/logo/logo.newyear2017.gif", "New Years (2017)"),
+            new Option("https://hackforums.net/images/logo/logo.nye2018.png", "New Years (2018)"),
+            new Option("https://hackforums.net/images/logo/logo.nye2019.png", "New Years (2019)"),
+            new Option("https://hackforums.net/images/logo/logo.nye2020.gif", "New Years (2020 FU)"),
+            new Option("https://hackforums.net/images/logo/logo.ny2020.png", "New Years (2020)"),
+            new Option("https://hackforums.net/images/logo/logo.thanksgiving.gif", "Thanksgiving (Classic)"),
+            new Option("https://hackforums.net/images/logo/logo.thankgiving2017.png", "Thanksgiving (2017)"),
+            new Option("https://hackforums.net/images/logo/logo.thanksgiving-2018.png", "Thanksgiving (2018)"),
+            new Option("https://hackforums.net/images/logo/logo.valentine.png", "Valentines Day (Classic)"),
+            new Option("https://hackforums.net/images/logo/logo.valentine2018.png", "Valentines Day (2018)")
+          ]})
       )
     });
   }
 
   run(settings) {
-    const themeName = Util.getConfigurableValue("tcThemeName", this, settings);
     let mosaic = null;
     let logo = null;
     let accentColor = null;
+    let isDefaultTheme = false;
     let css = "";
-    switch (themeName) {
+
+    switch (settings["tcThemeName"]) {
       case "cyan":
         logo = Util.getURL("/assets/images/theme-customizer/logos/logo_cyan.gif");
         mosaic = Util.getURL("/assets/images/theme-customizer/mosaics/mosaic_cyan.png");
@@ -92,35 +137,43 @@ class ThemeCustomizer extends Feature {
         accentColor = "linear-gradient(to right, red,orange,green,blue,purple)";
         css += ".contract_statusbar span.nav_con_active:after { background: purple !important; }";
         break;
-      // TODO: Make yellow less aggressive
       case "yellow":
         logo = Util.getURL("/assets/images/theme-customizer/logos/logo_yellow.gif");
         mosaic = Util.getURL("/assets/images/theme-customizer/mosaics/mosaic_yellow.png");
         accentColor = "#cab41b";
         break;
       default:
+        isDefaultTheme = true;
     }
 
-    if (mosaic && logo && accentColor) {
-      // logo
-      if (Util.getConfigurableValue("tcChangeLogo", this, settings)) {
-        $(".logo-a > img").attr("src", logo);
+    // logo
+    if (settings["tcChangeLogo"]) {
+      const logoImage = settings["tcLogoImage"];
+      // if previous HF logo
+      if (logoImage !== "default") {
+        $(".logo-a > img").attr("src", logoImage);
+        css += ".logo-img { width: auto; height: auto; } ";
+      } else {
+        if (!isDefaultTheme) {
+          $(".logo-a > img").attr("src", logo);
+        }
       }
-      // mosaic
-      if (Util.getConfigurableValue("tcChangeMosaic", this, settings)) {
-        $("body").css("background", `#0e0e0e url(${mosaic}) fixed`);
-      }
-      // background shadow
-      if (Util.getConfigurableValue("tcBackgroundShadow", this, settings)) {
-        css += "#container { background: rgb(11 5 11 / 70%) !important; }";
-      }
-      // accent color
-      css += `.thead, .shadetabs li a.selected, .pagination .pagination_current, .quickthread_button, .nav_con_active { background: ${accentColor} !important }
-        .nav_con_active span { background: none !important; }
-        .logo-hide-button { color: ${accentColor} !important; }`;
-
-      Util.addCssToPage(css);
     }
+    // mosaic
+    if (settings["tcChangeMosaic"] && !isDefaultTheme) {
+      $("body").css("background", `#0e0e0e url(${mosaic}) fixed`);
+    }
+    // background shadow
+    if (settings["tcBackgroundShadow"]) {
+      css += "#container { background: rgb(11 5 11 / 70%); }";
+    }
+    // accent color
+    if (accentColor) {
+      css += `.thead, .shadetabs li a.selected, .pagination .pagination_current, .quickthread_button, .nav_con_active { background: ${accentColor} }
+        .nav_con_active span { background: none; }
+        .logo-hide-button { color: ${accentColor}; }`;
+    }
+    Util.addCssToPage(css);
   }
 }
 

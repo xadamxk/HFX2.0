61904afcaa0b6c0d7db7b247cede5656d17966c7
Adam <xadamxk@gmail.com>
2021-07-17 10:17:00 -0500

    Auto slots ui changes

--- src/features/game/AutoSlots.js
+++ src/features/game/AutoSlots.js
@@ -35,7 +35,55 @@ class AutoSlots extends Feature {
     const currentPageUrl = location.href;
     // Don't do anything if not slots
     if (currentPageUrl.includes("/slots.php")) {
-      //
+      // Shrink slots UI td
+      $("#PageContainer").parent().css("width", $("#PageContainer").css("width"));
+      // Append stats td
+      $("#PageContainer").parent().after($("<td>").addClass("trow1")
+        .append($("<div>").css("height", $("#PageContainerInner").css("height"))
+          .append($("<div>").attr("id", "hfgsStatsContainer"))));
+      // Append warning
+      $("#hfgsStatsContainer").append($("<tr>").css("color", "red").text("HFX Warning: USE AT YOUR OWN RISK!"));
+      // Append slots history table
+      const tableAttributes = {
+        "border": "0",
+        "cellspacing": "0",
+        "cellpadding": "5",
+        "class": "tborder",
+        "margin": "10px auto",
+        "width": "100% !important",
+        "id": "historyTable" };
+      const tbodyCSS = {
+        "overflow-y": "auto",
+        "overflow-x": "hidden !important",
+        "max-height": "250px"
+      };
+      const trCSS = {};
+      $("#hfgsStatsContainer").append($("<table>").attr(tableAttributes)
+        .append($("<tbody>").attr("id", "historyTabletbody").css(tbodyCSS)
+          .append($("<tr>").css(trCSS)
+            .append($("<td>").addClass("thead").attr("colspan", "4")
+              .append($("<strong>").text("HF Slots Bot History"))))
+          .append($("<tr>").css(trCSS)
+            .append($("<td>").addClass("tcat").attr("colspan", "1")
+              .append($("<strong>").text("Result")))
+            .append($("<td>").addClass("tcat").attr("colspan", "1")
+              .append($("<strong>").text("Date")))
+            .append($("<td>").addClass("tcat").attr("colspan", "1")
+              .append($("<strong>").text("Wagered")))
+            .append($("<td>").addClass("tcat").attr("colspan", "1")
+              .append($("<strong>").text("Received")))
+          )
+        )
+      );
+
+      // // Append warning
+      // $("strong:contains(\"Risk your βytes for a chance to win more!\")").parent().parent().parent().find("tr:eq(2)")
+      //   .after($("<tr>").append($("<td>").addClass("trow1").attr({"id": "hfxSlotsContainer", "colspan": "2"})))
+      //   .after($("<tr>").css("color", "red").text("HFX Additions: USE AT YOUR OWN RISK!"));
+
+      // Append auto button
+      $("#hfgsStatsContainer").append($("<div>").css({"text-align": "center", "margin": "10px"})
+        .append($("<button>").attr({"title": "HFX Auto Spin", "id": "hfxAutoSpin"}).text("RUN AUTO SPIN")));
     }
   }
 };

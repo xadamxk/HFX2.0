91da91ba13ee192c2d0c971781bf03fcde4554e4
DeviceHF <90981844+DeviceHF@users.noreply.github.com>
2021-09-28 19:05:22 +0100

    Update ShowContractedReplies.js

--- src/features/threads/ShowContractedReplies.js
+++ src/features/threads/ShowContractedReplies.js
@@ -27,7 +27,7 @@ class ShowContractedReplies extends Feature {
       // Creates a "Contracted replies" button
       let button = document.createElement("button");
       button.innerHTML = "Contracted Posts";
-      button.className = "button new_reply_button";
+      button.className = "button contracted-posts";
       $(button).css({"padding": "11px", "background-color": "#1f1f1f", "border": "none", "font-size": "14px"});
       $(button).prependTo($(".float_right.mobile-remove"));
       button.onclick = function() {

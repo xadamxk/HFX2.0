b3cced9cc7e6eef93f7b44d538e15c71b911a538
Wessel Verheij <info@nightworks.io>
2021-11-08 20:31:08 +0100

    Add eggs to LinkedAwards

--- src/features/threads/LinkedAwards.js
+++ src/features/threads/LinkedAwards.js
@@ -7,7 +7,7 @@ class LinkedAwards extends Feature {
       section: Threads,
       name: "Linked Awards",
       default: true,
-      description: "Makes awards clickable if the description contains a link.",
+      description: "Make awards that are coupled to a specific thread/post linkable.",
       author: {
         name: "False King",
         profile: "https://hackforums.net/member.php?action=profile&uid=1665634"
@@ -19,13 +19,27 @@ class LinkedAwards extends Feature {
     document.querySelectorAll("div.post_myawards > span").forEach((awardsPostbit) => {
       awardsPostbit.querySelectorAll("i.award_sprite").forEach((award, i) => {
         const urlRegex = new RegExp(/\bhttps?:\/\/\S+/i);
+        const eggAwards = [
+          "award_137", // Emerald
+          "award_138", // Sapphire
+          "award_139", // Gold
+          "award_143" // Ruby
+        ];
+
+        let result = '';
 
         if (urlRegex.test(award.title)) {
           const matches = award.title.match(urlRegex);
+          result = matches[0];
+        } else if (eggAwards.includes(award.classList.item(1))) {
+          const postId = award.title.match(/\d+/g);
+          result = "https://hackforums.net/showthread.php?pid=" + postId + "#" + postId;
+        }
 
+        if (result.length > 0) {
           award.classList.add("hfx-linkable");
           award.style.cursor = "pointer";
-          award.dataset.hfxLinkable = matches[0];
+          award.dataset.hfxLinkable = result;
         }
       });
     });

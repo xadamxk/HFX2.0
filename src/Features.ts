628734a2c497c08fbf03520dabc2866897fa5b80
Adam Koewler <xadamxk@gmail.com>
2025-10-20 22:16:39 -0500

    chore: add interactive post stats feature

--- src/Features.ts
+++ src/Features.ts
@@ -1,11 +1,13 @@
 import HFToolbar from "./features/global/HFToolbar";
+import InteractivePostStats from "./features/threads/InteractivePostStats";
 import ShowPostNumbers from "./features/threads/ShowPostNumbers";
 import TrackingLinks from "./features/privatemessages/TrackingLinks";
 import test from "./features/global/test";
 
 export default {
   HFToolbar: HFToolbar,
+  InteractivePostStats: InteractivePostStats,
   ShowPostNumbers: ShowPostNumbers,
   TrackingLinks: TrackingLinks,
-  test: test,
+  test: test
 };

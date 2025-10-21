582475cd97f37ffb8b76c83fd06369be98bd3c2e
Adam Koewler <xadamxk@gmail.com>
2025-10-20 22:49:39 -0500

    chore: add posts on thread feature

--- src/Features.ts
+++ src/Features.ts
@@ -2,6 +2,7 @@ import ExpandIgnoredPosts from "./features/threads/ExpandIgnoredPosts";
 import HFToolbar from "./features/global/HFToolbar";
 import HideAwardRows from "./features/threads/HideAwardRows";
 import InteractivePostStats from "./features/threads/InteractivePostStats";
+import PostsOnThread from "./features/threads/PostsOnThread";
 import ShowPostNumbers from "./features/threads/ShowPostNumbers";
 import TrackingLinks from "./features/privatemessages/TrackingLinks";
 import test from "./features/global/test";
@@ -11,6 +12,7 @@ export default {
   HFToolbar: HFToolbar,
   HideAwardRows: HideAwardRows,
   InteractivePostStats: InteractivePostStats,
+  PostsOnThread: PostsOnThread,
   ShowPostNumbers: ShowPostNumbers,
   TrackingLinks: TrackingLinks,
   test: test

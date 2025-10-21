ded0df9e0678dbbe1e164b38ddc468f012b01537
Adam Koewler <xadamxk@gmail.com>
2025-10-20 22:45:46 -0500

    chore: add hide award row feature

--- src/Features.ts
+++ src/Features.ts
@@ -1,5 +1,6 @@
 import ExpandIgnoredPosts from "./features/threads/ExpandIgnoredPosts";
 import HFToolbar from "./features/global/HFToolbar";
+import HideAwardRows from "./features/threads/HideAwardRows";
 import InteractivePostStats from "./features/threads/InteractivePostStats";
 import ShowPostNumbers from "./features/threads/ShowPostNumbers";
 import TrackingLinks from "./features/privatemessages/TrackingLinks";
@@ -8,6 +9,7 @@ import test from "./features/global/test";
 export default {
   ExpandIgnoredPosts: ExpandIgnoredPosts,
   HFToolbar: HFToolbar,
+  HideAwardRows: HideAwardRows,
   InteractivePostStats: InteractivePostStats,
   ShowPostNumbers: ShowPostNumbers,
   TrackingLinks: TrackingLinks,

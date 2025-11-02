5a8db5602e49b43e717dda087fe6cbfa085b0830
Adam Koewler <xadamxk@gmail.com>
2025-11-01 21:48:30 -0500

    chore: add revert custom group stars option to group asset customizer

--- src/features/global/GroupAssetCustomizer.ts
+++ src/features/global/GroupAssetCustomizer.ts
@@ -39,12 +39,19 @@ class GroupAssetCustomizer extends Feature {
         new Checkbox({
           id: "resizeUserbars",
           label: "Resize Userbars",
-          description: "Resize the userbars to the original size.",
+          description: "Resize all userbars to the original size (136x42).",
+          default: false,
+        }),
+        new Checkbox({
+          id: "revertCustomStars",
+          label: "Revert Custom Group Stars",
+          description: "Revert all custom group stars to the original style (yellow stars).",
           default: false,
         }),
       ],
     });
   }
+  // Group map
   private currentUserbars: AssetGroup = {
     admin: "images/groupimages/admin-2.webp",
     staff: "images/groupimages/staff-2025.png",
@@ -53,19 +60,58 @@ class GroupAssetCustomizer extends Feature {
     vender: "images/groupimages/vendor.webp",
   };
 
+  private currentDefaultStars: AssetGroup = {
+    admin: "https://hackforums.net/images/mobale/staff_star.png",
+    staff: "https://hackforums.net/images/mobale/staff_star.png",
+    uber: "https://hackforums.net/images/mobale/ub3rstar.gif",
+    leet: "https://hackforums.net/images/mobale/star.gif",
+    vender: "https://hackforums.net/images/mobale/star-vendor.png",
+  };
+
+  // Class map
+  private customGroupStars = {
+    "group78": "https://hackforums.net/images/mobale/star-78.gif", // Casino
+    "group59": "https://hackforums.net/images/mobale/star-59b.gif", // Olympians
+    "group52":"https://hackforums.net/images/mobale/star-52.gif", // Pink LSZ
+    "group54":"https://hackforums.net/images/mobale/star-d2.png", // Succubus
+    "group48":"https://hackforums.net/images/mobale/star-48c.gif", // Quantum
+    "group50":"https://hackforums.net/images/mobale/star.gif", // Legends
+    "group71":"https://hackforums.net/images/mobale/star-71.gif", // Warriors
+    "group77":"https://hackforums.net/images/mobale/star-77.gif", // The Academy
+    "group56":"https://hackforums.net/images/mobale/star-56.gif", // Blacklisted
+    "group46":"https://hackforums.net/images/mobale/star-46e.gif", // H4ack3r$
+    "group57":"https://hackforums.net/images/mobale/star-57.png", // Lions League
+    "group53":"https://hackforums.net/images/mobale/star-53d.gif", // Eden
+    "group69":"https://hackforums.net/images/mobale/star-allegiance.gif", // Allegiance
+    "group70":"https://hackforums.net/images/mobale/star_Fv9.gif", // Gamblers
+    "group0":" https://hackforums.net/images/mobale/star.gif", // Equilibrium
+    "gradient-silver":"https://hackforums.net/images/star.png", // Brotherhood
+    "group49":"https://hackforums.net/images/mobale/star-red2.png", // Sociopaths
+    // "":"https://hackforums.net/images/star.png", // Terminal (has no class value)
+    "group23":"https://hackforums.net/images/mobale/star-mob.gif", // Mob
+    "group12":"https://hackforums.net/images/mobale/star-12.gif", // Benevolence
+    "group63":"https://hackforums.net/images/mobale/star-orange.png", // Infamous
+  }
+
   run(settings: any) {
     const theme = settings.theme;
     const resizeUserbars = settings.resizeUserbars;
-
+    const revertCustomStars = settings.revertCustomStars;
 
     if(resizeUserbars) {
       // Resize all current userbars to 136x42
-      // TODO: make sure this selector works for all userbars
-      const existingUserbars = document.querySelectorAll('img[src^="images/groupimages/custom/"]');
+      // Default group images are in images/groupimages/
+      // Custom group images are in images/groupimages/custom/
+      const existingUserbars = document.querySelectorAll('img[src^="images/groupimages/"]');
       existingUserbars.forEach((img: HTMLImageElement) => {
         img.style.width = "136px";
         img.style.height = "42px";
       });
+    }
+
+    if(revertCustomStars) {
+      console.log("Reverting custom stars");
+      this.replaceStar();
     }
 
     switch (theme) {
@@ -112,6 +158,31 @@ class GroupAssetCustomizer extends Feature {
         }
       });
     });
+  }
+
+  private replaceStar(){
+    // Only replace stars on showthreads.php
+    const currentUrl = window.location.pathname;
+    if(currentUrl.includes("/showthread.php")) {
+      // Replace stars
+      const posts = this.querySelectorAll(".post");
+      posts.forEach((post) => {
+        const authorInfo = post.querySelector(".author_information");
+        const userSpan = authorInfo?.querySelector("strong > span.largetext > a > span");
+        const groupClass = userSpan?.getAttribute("class");
+
+        // Replace stars if custom group match
+        if(groupClass && groupClass in this.customGroupStars) {
+          const starContainer = authorInfo?.querySelector("span.smalltext");
+          const stars = starContainer?.querySelectorAll("img");
+          stars?.forEach((star: HTMLImageElement) => {
+            if(star.src.includes("https://hackforums.net/images/mobale/")) {
+              star.src = this.currentDefaultStars.leet;
+            }
+          });
+        }
+      });
+    }
   }
 }
 

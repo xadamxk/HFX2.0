66764b88a4b82b5a99ada028d1b2018539c7b364
Adam Koewler <xadamxk@gmail.com>
2025-11-17 22:13:29 -0600

    feat: add popularity section

--- src/Sections.ts
+++ src/Sections.ts
@@ -6,6 +6,7 @@ export default {
   Global: require("./sections/Global"),
   Groups: require("./sections/Groups"),
   HFX: require("./sections/HFX"),
+  Popularity: require("./sections/Popularity"),
   PrivateMessages: require("./sections/PrivateMessages"),
   Profile: require("./sections/Profile"),
   Threads: require("./sections/Threads")
